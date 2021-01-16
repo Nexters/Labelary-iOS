e777d205aa4dc5c4536512ee9d203370a84756db
freemjstudio <mjwoo0@naver.com>
2021-01-16 17:35:26 +0900

    Initial Commit

---
+++ Fullstack/FullstackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullstackApp.swift
+//  Fullstack
+//
+//  Created by 우민지 on 2021/01/16.
+//
+
+import SwiftUI
+
+@main
+struct FullstackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
