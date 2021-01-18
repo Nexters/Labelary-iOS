9ce251d955d7717676aa33ff0dd5c260228243d7
freemjstudio <mjwoo0@naver.com>
2021-01-18 23:58:52 +0900

    [ADD] MainLabelingView CardStack

--- Fullstack/Model/Model.swift
+++ Fullstack/Model/Model.swift
@@ -3,6 +3,6 @@
 //  Fullstack
 //
 //  Created by 우민지 on 2021/01/16.
-//
+
 
 import Foundation
--- Fullstack/View/MainLabelingView.swift
+++ Fullstack/View/MainLabelingView.swift
@@ -6,15 +6,70 @@
 //
 
 import SwiftUI
+import CardStack
+
+struct Photo: Identifiable {
+    let id = UUID()
+    let image: UIImage
+    
+    static let mock: [Photo] = [
+        Photo(image: UIImage(named: "1")!),
+        Photo(image: UIImage(named: "2")!),
+        Photo(image: UIImage(named: "3")!),
+        Photo(image: UIImage(named: "4")!),
+        Photo(image: UIImage(named: "5")!)
+    ]
+}
+
+
+struct CardView: View {
+    let photo: Photo
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                Image(uiImage: self.photo.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: geo.size.width)
+                    .clipped()
+            }
+            .cornerRadius(12)
+            .shadow(radius: 4)
+        }
+    }
+}
+
 
 struct MainLabelingView: View {
+    @State var data: [Photo] = Photo.mock
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CardStack(
+            direction: LeftRight.direction,
+            data: data,
+            onSwipe: { card, direction in
+                print("Swiped \(card.image) to \(direction)")
+            },
+            content: { photo,_,_  in
+                CardView(photo: photo) 
+            }
+        )
+        .padding()
+        .scaledToFit()
+        //.frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
 }
 
+    
 struct MainLabelingView_Previews: PreviewProvider {
     static var previews: some View {
-        MainLabelingView()
+        TabView {
+            MainLabelingView()
+                .tabItem { Image(systemName: "photo.fill")
+                Text("라벨링")
+                }
+        }
+       
     }
 }
+ 
