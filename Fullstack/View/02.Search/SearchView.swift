ecaa828d11864ba22bf3fd2055fbf91a94e13881
freemjstudio <mjwoo0@naver.com>
2021-01-22 23:19:14 +0900

    [Edit]SearchBar

--- Fullstack/View/02.Search/SearchView.swift
+++ Fullstack/View/02.Search/SearchView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            SearchBar(text: .constant(""))
+              
+            Spacer()
+        }
+     
+        
     }
 }
 
