fa840ad2e758bffeff23028502cd16684d3df4eb
omjoonkim <>
2021-01-26 23:58:51 +0900

    chore : Add missing code from previous commit

--- Fullstack/Foundation/ScreenLoadingType.swift
+++ Fullstack/Foundation/ScreenLoadingType.swift
@@ -1,10 +1,3 @@
-//
-//  ScreenLoadingType.swift
-//  CleanArchitecture
-//
-//  Created by Tuan Truong on 8/7/20.
-//  Copyright © 2020 Tuan Truong. All rights reserved.
-//
 
 public enum ScreenLoadingType<Input> {
     case loading(Input)
