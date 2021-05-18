44d1390a4541c4f4b8427cfaf91de715c95165e6
freemjstudio <mjwoo0@naver.com>
2021-05-18 20:50:06 +0900

    ShareScreen - getImage

--- ShareSheet/ShareViewController.swift
+++ ShareSheet/ShareViewController.swift
@@ -11,24 +11,27 @@ import UIKit
 
 @objc(ShareViewController)
 class ShareViewController: UIViewController {
-    @IBOutlet weak var container: UIView!
-    
+    @IBOutlet var container: UIView!
+    // @ObservedObject var model = extensionOutput()
+
     var vc = UIHostingController(rootView: LabelViewFromOutside())
+
+    override func viewWillAppear(_ animated: Bool) {}
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        getImage()
         setupNavBar()
         addChild(vc)
         vc.view.frame = container.bounds
         container.addSubview(vc.view)
         vc.didMove(toParent: self)
-        
-        
-        self.view.backgroundColor = .systemGray6
-        
-       // self.setupNavBar()
-        self.setUpConstraints()
+
+        view.backgroundColor = .systemGray6
+
+        setUpConstraints()
     }
-    
+
     func setUpConstraints() {
         vc.view.translatesAutoresizingMaskIntoConstraints = false
         vc.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
@@ -36,18 +39,36 @@ class ShareViewController: UIViewController {
         vc.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         vc.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
-    
-    
+
+    // MARK: - Get Image file from share extension
+
+    func getImage() {
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeData as String) {
+                    itemProvider.loadItem(forTypeIdentifier: kUTTypeData as String) {
+                        [unowned self] imageData, _ in
+                        if let item = imageData as? Data {
+                            // self.model.imageData = UIImage(data: item)
+                            // print(self.model.imageData)
+                            // self.imageView.image = UIImage(data: item)
+                            print(UIImage(data: item))
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     // 2: Set the title and the navigation items
     private func setupNavBar() {
-        self.navigationItem.title = "???"
+        navigationItem.title = "???"
 
-        let itemCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.cancelAction))
-        self.navigationItem.setLeftBarButton(itemCancel, animated: false)
+        let itemCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelAction))
+        navigationItem.setLeftBarButton(itemCancel, animated: false)
 
-        let itemDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.doneAction))
-        self.navigationItem.setRightBarButton(itemDone, animated: false)
+        let itemDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        navigationItem.setRightBarButton(itemDone, animated: false)
     }
 
     // 3: Define the actions for the navigation items
@@ -63,12 +84,11 @@ class ShareViewController: UIViewController {
 
 @objc(ShareNavigationController)
 class ShareNavigationController: UINavigationController {
-
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 
         // 2: set the ViewControllers
-        self.setViewControllers([ShareViewController()], animated: false)
+        setViewControllers([ShareViewController()], animated: false)
     }
 
     @available(*, unavailable)
@@ -76,3 +96,10 @@ class ShareNavigationController: UINavigationController {
         super.init(coder: aDecoder)
     }
 }
+
+// class extensionOutput: ObservableObject {
+//    @Published var imageData: UIImage
+//    init(imageData: UIImage) {
+//        self.imageData = imageData
+//    }
+// }
