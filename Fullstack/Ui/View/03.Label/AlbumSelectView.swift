1465712d84271f216e549b0479f6034b6e9277b1
freemjstudio <mjwoo0@naver.com>
2021-11-19 15:03:01 +0900

    [album] delete images 추가!

--- Fullstack/Ui/View/03.Label/AlbumSelectView.swift
+++ Fullstack/Ui/View/03.Label/AlbumSelectView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 // 스크린샷 선택
 struct AlbumSelectView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State private var selectedImages: [ImageEntity] = []
+    @State private var onTapped: Bool = false
+    @ObservedObject private var viewModel = ViewModel()
+
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
         ZStack {
             Color.DEPTH_4_BG.edgesIgnoringSafeArea(.all)
             VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 14) {
+                        ForEach(viewModel.screenshots.indices, id: \.self) { i in
+                            let screenshot = viewModel.screenshots[i]
+                            AlbumGridItem(selectedImages: $selectedImages, screenshot: screenshot, img: viewModel.unlabeledImages[i])
+                        }
+                    }
+                }
             }
         }.navigationBarBackButtonHidden(true)
             .navigationBarItems(leading:
@@ -21,19 +38,60 @@ struct AlbumSelectView: View {
                     Button(action: {
                         presentationMode.wrappedValue.dismiss()
                     }) { Image("ico_cancel") }
+                    
+                    Text("\(selectedImages.count)개")
+                        .font(Font.B1_BOLD)
+                        .foregroundColor(Color.PRIMARY_1)
+                        .opacity(selectedImages.count > 0 ? 1 : 0)
+                    
                 },
                 trailing: HStack {
                     // 라벨 수정하기 -> 스크린샷 라벨 변경 화면으로 이동
                     NavigationLink(destination: AlbumEditLabelView()) {
-                        Image("ico_label_edit")
-                    }
+                        selectedImages.count > 0 ?
+                            Image("ico_label_edit_active") : Image("ico_label_edit_inactive")
+                        
+                    }.disabled(selectedImages.count > 0 ? true : false)
               
                     // 이미지 삭제하기
-                    Button(action: {}) { Image("ico_delete_inactive") }
+                    Button(action: {
+                        viewModel.requestDeleteImage(selectedImages: selectedImages)
+                    }) {
+                        selectedImages.count > 0 ?
+                            Image("ico_delete_active") : Image("ico_delete_inactive")
+                    }.disabled(selectedImages.count > 0 ? true : false)
                 })
     }
     
     class ViewModel: ObservableObject {
         let deleteImage = DeleteImages(imageRepository: ImageRepositoryImpl(cachedDataSource: CachedData()))
+    
+        @Published var unlabeledImages: [ImageEntity] = []
+        @Published var screenshots: [ImageViewModel] = []
+        @Published var currentLabel: [LabelEntity] = []
+   
+        let loadLabelingData = LoadLabelingData(imageRepository: ImageRepositoryImpl(cachedDataSource: CachedData()))
+        let requestLabeling = RequestLabeling(imageRepository: ImageRepositoryImpl(cachedDataSource: CachedData()))
+        let cancelBag = CancelBag()
+    
+        init() {
+            loadLabelingData.get().sink(receiveCompletion: {
+                _ in
+            }, receiveValue: { [self] data in
+                self.unlabeledImages.append(contentsOf: data)
+                self.setImages(unlabeledImages: unlabeledImages)
+            }).store(in: cancelBag)
+            currentLabel.append(passingLabelEntity.selectedLabel!)
+        }
+    
+        func setImages(unlabeledImages: [ImageEntity]) {
+            screenshots = unlabeledImages.map {
+                ImageViewModel(image: $0)
+            }
+        }
+        
+        func requestDeleteImage(selectedImages: [ImageEntity]) {
+            deleteImage.get(param: selectedImages).sink(receiveCompletion: { _ in }, receiveValue: { _ in }).store(in: cancelBag)
+        }
     }
 }
