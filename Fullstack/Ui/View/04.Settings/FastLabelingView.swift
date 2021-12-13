6a01eaa8f65e265b08620da2a35e6004bc100295
freemjstudio <mjwoo0@naver.com>
2021-12-13 13:53:51 +0900

    setting view + url + share sheet

---
+++ Fullstack/Ui/View/04.Settings/FastLabelingView.swift
@@ -0,0 +1,17 @@
+//
+//  FastLabelingView.swift
+//  Fullstack
+//
+//  Created by 우민지 on 2021/12/13.
+//
+
+import SwiftUI
+
+// 빠른 라벨링 설정하기
+
+struct FastLabelingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
--- Fullstack/Ui/View/04.Settings/SettingView.swift
+++ Fullstack/Ui/View/04.Settings/SettingView.swift
@@ -5,17 +5,28 @@
 //  Created by 우민지 on 2021/12/12.
 //
 
+import MessageUI
 import SwiftUI
+import UIKit
 
 struct SettingView: View {
     @Environment(\.presentationMode) var presentationMode
-    
+    @ObservedObject private var viewModel = ViewModel()
+    @State private var showMailView = false
+    @State private var showAlert = false
+    @State private var mailData = ComposeMailData(subject: "Report", receivers: ["mjwoo001@gmail.com"], message: " write message...", attachments: [AttachmentData(data: "text".data(using: .utf8)!, mimeType: "text/plain", fileName: "text.txt")])
+
     var body: some View {
         ZStack {
             Color.DEPTH_4_BG.edgesIgnoringSafeArea(.top)
             Color.DEPTH_5.edgesIgnoringSafeArea(.bottom)
             VStack {
-                Button(action: {}, label: {
+                Button(action: {
+                    let url = URL(string: "itms://itunes.apple.com/de/app/x-gift/id839686104?mt=8&uo=4")
+                    if UIApplication.shared.canOpenURL(url!) {
+                        UIApplication.shared.openURL(url!)
+                    }
+                }, label: {
                     Text("앱스토어에 리뷰쓰기")
                         .font(Font.B1_MEDIUM).foregroundColor(Color.PRIMARY_1)
                         .padding(.leading, 20)
@@ -25,7 +36,11 @@ struct SettingView: View {
                 }).frame(width: UIScreen.main.bounds.width, height: 80, alignment: .leading)
                     .background(Color.DEPTH_4_BG)
 
-                Button(action: {}, label: {
+                Button(action: {
+                    guard let urlShare = URL(string: "https://developer.apple.com/xcode/swiftui/") else { return }
+                    let activityVC = UIActivityViewController(activityItems: [urlShare], applicationActivities: nil)
+                    UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+                }, label: {
                     Text("친구에게 추천하기")
                         .font(Font.B1_MEDIUM).foregroundColor(Color.PRIMARY_1)
                         .padding(.leading, 20)
@@ -37,7 +52,13 @@ struct SettingView: View {
                     .padding(.bottom, 20)
                     .padding(.top, -9)
 
-                Button(action: {}, label: {
+                Button(action: {
+                    let url = URL(string: "https://www.notion.so/bf4b20126ab64757856abfcb9db9c66d")
+                    if UIApplication.shared.canOpenURL(url!) {
+                        UIApplication.shared.openURL(url!)
+                    }
+
+                }, label: {
                     Text("이용방법 보기")
                         .font(Font.B1_MEDIUM).foregroundColor(Color.PRIMARY_1)
                         .padding(.leading, 20)
@@ -57,7 +78,9 @@ struct SettingView: View {
                 }).frame(width: UIScreen.main.bounds.width, height: 80, alignment: .leading)
                     .background(Color.DEPTH_4_BG)
                     .padding(.top, -9)
-                Button(action: {}, label: {
+                Button(action: {
+                    showMailView.toggle()
+                }, label: {
                     Text("피드백 보내기")
                         .font(Font.B1_MEDIUM).foregroundColor(Color.PRIMARY_1).padding(.leading, 20)
                     Spacer()
@@ -67,6 +90,12 @@ struct SettingView: View {
                     .background(Color.DEPTH_4_BG)
                     .padding(.bottom, 20)
                     .padding(.top, -9)
+                    .disabled(!MailView.canSendMail)
+                    .sheet(isPresented: $showMailView) {
+                        MailView(data: $mailData) { result in
+                            print(result)
+                        }
+                    }
 
                 Button(action: {
                     // label 초기화
@@ -75,7 +104,7 @@ struct SettingView: View {
                         Text("라벨 초기화하기")
                             .foregroundColor(Color.LABEL_RED_ACTIVE)
                             .font(Font.B1_MEDIUM)
-                            .padding(.bottom, 1)
+                            .padding(.bottom, 0.5)
                         Text("라벨 내역이 초기화되며, 스크린샷은 삭제되지 않습니다.")
                             .foregroundColor(Color.PRIMARY_2)
                             .font(Font.B2_REGULAR)
@@ -94,18 +123,91 @@ struct SettingView: View {
             .navigationBarItems(leading:
 
                 HStack {
-                    Button(action:  {
+                    Button(action: {
                         self.presentationMode.wrappedValue.dismiss()
                     }, label: {
                         Image("navigation_back_btn")
                     })
-                    .padding(.leading, -12)
+                        .padding(.leading, -12)
                 }
             )
         }
     }
-    
+
     class ViewModel: ObservableObject {
-        
+        // let removeAllLabel
+    }
+}
+
+struct ComposeMailData {
+    let subject: String
+    let receivers: [String]?
+    let message: String
+    let attachments: [AttachmentData]?
+}
+
+struct AttachmentData {
+    let data: Data
+    let mimeType: String
+    let fileName: String
+}
+
+typealias MailViewCallback = ((Result<MFMailComposeResult, Error>) -> Void)?
+
+struct MailView: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentation
+    @Binding var data: ComposeMailData
+    let callback: MailViewCallback
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        @Binding var presentation: PresentationMode
+        @Binding var data: ComposeMailData
+        let callback: MailViewCallback
+
+        init(presentation: Binding<PresentationMode>,
+             data: Binding<ComposeMailData>,
+             callback: MailViewCallback)
+        {
+            _presentation = presentation
+            _data = data
+            self.callback = callback
+        }
+
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?)
+        {
+            if let error = error {
+                callback?(.failure(error))
+            } else {
+                callback?(.success(result))
+            }
+            $presentation.wrappedValue.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(presentation: presentation, data: $data, callback: callback)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.mailComposeDelegate = context.coordinator
+        vc.setSubject(data.subject)
+        vc.setToRecipients(data.receivers)
+        vc.setMessageBody(data.message, isHTML: false)
+        data.attachments?.forEach {
+            vc.addAttachmentData($0.data, mimeType: $0.mimeType, fileName: $0.fileName)
+        }
+
+        vc.accessibilityElementDidLoseFocus()
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {}
+
+    static var canSendMail: Bool {
+        MFMailComposeViewController.canSendMail()
     }
 }
